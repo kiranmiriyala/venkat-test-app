858adfe54cac4dbbb618d6adc96c06097e39249f
Miryala <vmiryal@americas.corpdir.net>
2023-05-12 09:30:06 -0400

    first commit

---
+++ src/main/java/com/example/venkattestapp/VenkatTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.venkattestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenkatTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenkatTestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/venkattestapp/VenkatTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.venkattestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenkatTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
